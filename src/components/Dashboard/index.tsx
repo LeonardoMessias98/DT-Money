73a63cbb3b6721c995aad331a74dc591d4744667
leonardo messias <messiasleonardo98@gmail.com>
2021-05-23 21:43:54 -0300

    feat(transactions_dashboard): implementing first screen with static data

---
+++ src/components/Dashboard/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import Summary from 'components/Summary'
+import TransactionsTable from 'components/TransactionsTable'
+
+import { Container } from './styles'
+
+const Dashboard = () => {
+  return (
+    <Container>
+      <Summary />
+      <TransactionsTable />
+    </Container>
+  )
+}
+
+export default Dashboard
